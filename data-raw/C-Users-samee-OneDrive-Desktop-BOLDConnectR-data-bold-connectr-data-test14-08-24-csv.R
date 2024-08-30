e9c0cd5703d4b156228fb8fa85d61cadf1408466
Sameer Padhye <sameer.m.padhye@gmail.com>
2024-08-30 13:21:33 -0400

    update raw data removed and readme
    added new github install url for beta release

--- data-raw/C-Users-samee-OneDrive-Desktop-BOLDConnectR-data-bold-connectr-data-test14-08-24-csv.R
+++
@@ -1,3 +0,0 @@
-## code to prepare `C:/Users/samee/OneDrive/Desktop/BOLDConnectR/data/bold.connectr.data.test14-08-24.csv` dataset goes here
-
-usethis::use_data(C:/Users/samee/OneDrive/Desktop/BOLDConnectR/data/bold.connectr.data.test14-08-24.csv, overwrite = TRUE)
