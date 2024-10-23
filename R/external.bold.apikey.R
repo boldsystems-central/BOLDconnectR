1ab8596242d7505d0d1e8234fe00490a5c3f2796
Sameer Padhye <sameer.m.padhye@gmail.com>
2024-10-23 14:14:01 -0400

    apikey function update
    details added

--- R/external.bold.apikey.R
+++ R/external.bold.apikey.R
@@ -5,7 +5,8 @@
 #'
 #' @param apikey A character string required for authentication and data access.
 #'
-#' @details `bold.apikey` creates a variable called `apikey` that stores the access token provided by BOLD. This apikey variable is then used internally by the [bold.fetch()] function, so that the user does not have need to input it again. To set the `apikey`, the token must be provided as an input for the function before any other functions are called. The api_key is a UUID v4 hexadecimal string obtained upon request from BOLD at `support@boldsystems.org` and is valid for one year,  after which it must be renewed.
+#' @details `bold.apikey` creates a variable called `apikey` that stores the access token provided by BOLD. This apikey variable is then used internally by the [bold.fetch()] function, so that the user does not have need to input it again. To set the `apikey`, the token must be provided as an input for the function before any other functions are called. The api_key is a UUID v4 hexadecimal string and is valid for one year,  after which it must be renewed.
+#' \emph{Obtaining the API key}: The API key is found in the BOLD 'Workbench'<https://bench.boldsystems.org/index.php/Login/page?destination=MAS_Management_UserConsole>. After logging in, navigate to 'Your Name' (located at the top left-hand side of the window) and click 'Edit User Preferences'. You can find the API key in the 'User Data' section. Please note that to have an API key available in the workbench, a user must have uploaded at least 10,000 records to BOLD.
 #'
 #' @returns Token saved as 'apikey'
 #'
